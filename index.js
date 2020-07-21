005b480f296333f79bfc396d62a6fbd5be8dd53b
Maddie <55847867+MaddieAndreea23@users.noreply.github.com>
2020-07-21 07:46:10 +0200

    Mongoose

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const uuid = require("uuid");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+// Mongoose //
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/MyFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 let movies = [
   {
@@ -14,7 +22,7 @@ let movies = [
       "Set in the fictional world of Middle-earth, the films follow the hobbit Frodo Baggins as he and the Fellowship embark on a quest to destroy the One Ring, to ensure the destruction of its maker, the Dark Lord Sauron.",
     genre: "Adventure",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 2,
@@ -24,7 +32,7 @@ let movies = [
       "The plot revolves around King Leonidas (Gerard Butler), who leads 300 Spartans into battle against the Persian 'God-King' Xerxes (Rodrigo Santoro) and his invading army of more than 300,000 soldiers. As the battle rages, Queen Gorgo (Lena Headey) attempts to rally support in Sparta for her husband. ",
     genre: "Action",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 3,
@@ -34,7 +42,7 @@ let movies = [
       "In London, a real-estate scam puts millions of pounds up for grabs, attracting some of the city's scrappiest tough guys and its more established underworld types, all of whom are looking to get rich quick.",
     genre: "Crime",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 4,
@@ -44,7 +52,7 @@ let movies = [
       "Will Hunting, a janitor at M.I.T., has a gift for mathematics, but needs help from a psychologist to find direction in his life.",
     genre: "Drama",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 5,
@@ -54,7 +62,7 @@ let movies = [
       "Oakland A's general manager Billy Beane's successful attempt to assemble a baseball team on a lean budget by employing computer-generated analysis to acquire new players.",
     genre: "Biography",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 6,
@@ -64,7 +72,7 @@ let movies = [
       "The story of how George Jung, along with the Medellín Cartel headed by Pablo Escobar, established the American cocaine market in the 1970s in the United States.",
     genre: "Crime",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 7,
@@ -74,7 +82,7 @@ let movies = [
       "Murphy is an American living in Paris who enters a highly emotionally charged relationship with the unstable Electra. Unaware of the effect it will have on their relationship, they invite their pretty neighbor into their bed.",
     genre: "Drama",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 8,
@@ -84,7 +92,7 @@ let movies = [
       "An American drug dealer living in Tokyo is betrayed by his best friend and killed in a drug deal. His soul, observing the repercussions of his death, seeks resurrection.",
     genre: "Fantasy",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 9,
@@ -94,7 +102,7 @@ let movies = [
       "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
     genre: "Crime",
     image: "",
-    featured: ""
+    featured: "true"
   },
   {
     id: 10,
@@ -104,7 +112,7 @@ let movies = [
       "When a simple jewelry heist goes horribly wrong, the surviving criminals begin to suspect that one of them is a police informant.",
     genre: "Crime",
     image: "",
-    featured: ""
+    featured: "true"
   }
 ];
 
@@ -202,25 +210,24 @@ let directors = [
 let users = [
   {
     id: 1,
-    name: "Jessica Drake",
-    username: "jessdrake2",
-    password: "drake1",
-    email: "jessica.drake@gmail.com",
-    birthday: "03-01-1992"
+    Name: "Jessica Drake",
+    Password: "drake1",
+    Email: "jessica.drake@gmail.com",
+    Birthday: "03-01-1992"
   },
   {
     id: 2,
-    name: "Ben Cohen",
-    password: "cohenben143",
-    email: "ben.cohen@gmail.com",
-    birthday: "23-06-1992"
+    Name: "Ben Cohen",
+    Password: "cohenben143",
+    Email: "ben.cohen@gmail.com",
+    Birthday: "23-06-1992"
   },
   {
     id: 3,
-    name: "Lisa Downing",
-    password: "downinglisa34",
-    email: "lisa.downing@gmail.com",
-    birthday: "11-12-1991"
+    Name: "Lisa Downing",
+    Password: "downinglisa34",
+    Email: "lisa.downing@gmail.com",
+    Birthday: "11-12-1991"
   }
 ];
 
@@ -244,81 +251,203 @@ app.get("/documentation", function(req, res) {
 
 /////////             =============== [ M O V I E S ] ===============            /////////
 
-
-// Gets the list of data about ALL movies
-app.get("/movies", (req, res) => {
-  res.json(movies);
-});
-
-// Gets the data about a single movie, by title
-app.get("/movies/:title", (req, res) => {
-  res.json(movies.find((movie) =>
-  { return movie.title === req.params.title }));
-});
-
-// Gets the list of data about ALL genres
-app.get("/genres", function(req, res) {
-  res.json(genres);
-});
-
-// Gets the data about a single genre, by name
-app.get("/genres/:name", (req, res) => {
-  res.json(
-    genres.find(genre => {
-      return genre.name === req.params.name;
+// Get all movies
+app.get('/movies', (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
     })
-  );
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-// Gets the list data about ALL directors
-app.get("/directors", function(req, res) {
-  res.json(directors);
-});
-// Gets the data about a single director, by name
-app.get("/directors/:name", (req, res) => {
-  res.json(
-    directors.find(director => {
-      return director.name === req.params.name;
+
+// Get a movie by title
+app.get('/movies/:title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((Title) => {
+      res.json(Title);
     })
-  );
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
+
+// Get all genres
+app.get('/genres', (req, res) => {
+  genres.find()
+    .then((genres) => {
+      res.status(201).json(genres);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Genre by name
+app.get('/movies/genres/:Name', (req, res) => {
+  Movies.findOne({'Genre.Name': req.params.Name})
+  .then(function(movie){
+    res.json(movie)
+    })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error:" + err);
+  });
+});
+
+
+// Gets all directors
+app.get('/directors', (req, res) => {
+  directors.find()
+    .then((directors) => {
+      res.status(201).json(directors);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Director by name
+app.get('/movies/directors/:Name', (req, res) => {
+  Movies.findOne({'Director.Name': req.params.Name})
+  .then(function(movie){
+    res.json(movie)
+    })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error:" + err);
+  });
+});
+
 
 /////////             =============== [ U S E R S ] ===============            /////////
 
 
-//Gets the list of data about ALL users
-app.get("/users", function(req, res) {
-  res.json(users);
-});
-// Gets the data about a single user, by name
-app.get("/users/:name", (req, res) => {
-  res.json(
-    users.find(user => {
-      return user.name === req.params.name;
+// Get all Users
+app.get('/Users', (req, res) => {
+  Users.find()
+    .then((Users) => {
+      res.status(201).json(Users);
     })
-  );
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
-// Adds data for a new user
+
+
+// Get a User by name
+app.get('/Users/:Name', (req, res) => {
+  Users.findOne({ Name: req.params.Name })
+    .then((User) => {
+      res.json(User);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+
+//Add a User
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
 app.post('/users', (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.name) {
-    const message = 'Missing name in request body';
-    res.status(400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).send(newUser);
-  }
+  users.findOne({ Name: req.body.Name })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Name + 'already exists');
+      } else {
+        users
+          .create({
+            Name: req.body.Name,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
-// Deletes a user from our list by id
-app.delete('/users/:id', (req, res) => {
-  let user = users.find((user) => { return user.id === req.params.id });
 
-  if (user) {
-    users = users.filter((obj) => { return obj.id !== req.params.id });
-    res.status(201).send('User ' + req.params.id + ' was deleted.');
-  }
+// Update a User's info, by name
+/* We’ll expect JSON in this format
+{
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+app.put('/Users/:Name', (req, res) => {
+  Users.findOneAndUpdate({ Name: req.params.Name }, { $set:
+    {
+      Name: req.body.Name,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+// Add a movie to a user's list of favorites
+app.post('/users/:Name/Movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Name: req.params.Name }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+// Delete a user by name
+app.delete('/users/:Name', (req, res) => {
+  Users.findOneAndRemove({ Name: req.params.Name })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.name + ' was not found');
+      } else {
+        res.status(200).send(req.params.name + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // error-handling middleware function that will log all application-level errors to the terminal
